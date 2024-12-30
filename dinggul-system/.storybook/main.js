88f6e8648979599212fd589989b5955293b28700
jinA <jinaj777@gmail.com>
2024-12-30 11:34:38 +0900

    1차 배포 준비

--- dinggul-system/.storybook/main.js
+++ dinggul-system/.storybook/main.js
@@ -20,5 +20,12 @@ const config = {
     isFullscreen: false,
     panelPosition: 'right',
   },
+  viteFinal: async config => {
+    // GitHub Pages를 위한 base URL 설정
+    if (process.env.BUILD_MODE === 'production') {
+      config.base = '/Dinggul/';
+    }
+    return config;
+  },
 };
 export default config;
